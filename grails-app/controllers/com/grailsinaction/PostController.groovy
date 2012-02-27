10110618d4165dbef2f247271c4f1e150c27debc
brianpedalsteel <brianpedalsteel@192.168.1.106>
2012-02-26 23:03:03 -0500

    page 129 flash scope

--- grails-app/controllers/com/grailsinaction/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -8,4 +8,25 @@ class PostController {
     def user = User.findByUserId(params.id)
     [user:user]
     }
+    
+    def addPost = {
+        
+        def user = User.findByUserId(params.id)
+        if(user){
+            def post = new Post(params)
+            user.addToPosts(post)
+            if(user.save()){
+                flash.message="Successfully created Post"
+            }else{
+                user.discard()
+                flash.message= "Field cannot be blank"
+                
+            }
+            redirect (action:'timeline',id: params.id)
+            
+        }
+        
+        
+        
+    }
 }
--- grails-app/views/post/timeline.gsp
+++ grails-app/views/post/timeline.gsp
@@ -6,6 +6,25 @@
       <meta name="layout" content="main"/> 
    </head>
 <body>
+  
+  <div id="newPost">
+<h3>
+What is ${user.profile.fullName} hacking on right now?
+</h3>
+
+ <g:if test="${flash.message}"> <div class="flash">
+      ${flash.message}
+   </div>
+</g:if>   
+
+<p>
+<g:form action="addPost" id="${params.id}">
+<g:textArea id='postContent' name="content" rows="3" cols="50"/><br/>
+<g:submitButton name="post" value="Post"/> </g:form>
+</p> </div>
+  
+  
+  
 <h1>Timeline for ${user.profile.fullName}</h1>
 
 <div class="allPosts"> 
