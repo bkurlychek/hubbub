52c54d084f893c89bbf61b6cd50c59ac7a2ed947
Brian Kurlychek <brian.kurlychek@maine.edu>
2012-02-26 20:34:39 -0500

    end of ch4/ starting 5.1

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,32 @@
+import com.grailsinaction.*
+import grails.util.*
+
 class BootStrap {
 
     def init = { servletContext ->
+        switch (Environment.current){
+            case Environment.DEVELOPMENT:
+            CreateAdminUserIfRequired()
+            break;
+            
+            case Environment.PRODUCTION:
+            println"No special configuration required."
+            break;
+        }
     }
     def destroy = {
+    }
+    
+    void createAdminUserifRequired(){
+        
+        if(!User.findByUserId("admin")){
+            println"fresh database, creating admin user."
+            def profile = new Profile (email: "admin@hubbub.com")
+            def user = new User(userId: "admin", 
+                password: "secret", profile:profile).save() 
+        }else{
+            println "existing admin user, skipping creation."
+        }
+        
     }
 }
